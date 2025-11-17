2aeb1247e3994efc91dc9ac5ee66f5b9f8241072
vi <victor.dosca@yandex.ru>
2025-11-17 08:22:41 +0300

    Don't let users on prices page unless they are logged in

--- app/prices/page.tsx
+++ app/prices/page.tsx
@@ -1,7 +1,14 @@
 import { Prices } from "@/components/prices";
 import { auth } from "../(auth)/auth";
+import { redirect } from "next/navigation";
 
 export default async function PricesPage() {
   const session = await auth();
+
+  console.log(session);
+  if (!session || session.user.type === "guest") {
+    redirect("/login");
+  }
+
   return <Prices session={session} />;
 }
